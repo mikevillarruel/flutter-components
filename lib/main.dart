b64681c44c32ef19b99e49e9960d2c094d5ac2a0
Michael Villarruel <alejovillarruel@gmail.com>
2021-10-13 21:19:00 -0500

    ListView

--- lib/main.dart
+++ lib/main.dart
@@ -5,17 +5,14 @@ import 'package:flutter_components/src/pages/home_temp.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       title: 'Components App',
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Components'),
-        ),
-        body: const HomePagaTemp(),
-      ),
+      home: HomePageTemp(),
     );
   }
 }
--- lib/src/pages/home_temp.dart
+++ lib/src/pages/home_temp.dart
@@ -1,12 +1,29 @@
 import 'package:flutter/material.dart';
 
-class HomePagaTemp extends StatelessWidget {
-  const HomePagaTemp({Key? key}) : super(key: key);
+class HomePageTemp extends StatelessWidget {
+  const HomePageTemp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Home Temp'),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Components Temp'),
+      ),
+      body: ListView(
+        children: const <Widget>[
+          ListTile(
+            title: Text('ListTile Title'),
+          ),
+          Divider(),
+          ListTile(
+            title: Text('ListTile Title'),
+          ),
+          Divider(),
+          ListTile(
+            title: Text('ListTile Title'),
+          ),
+        ],
+      ),
     );
   }
 }
