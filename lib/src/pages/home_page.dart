2a2ca1788ced00939a31d43b3dfe495630386f85
Michael Villarruel <alejovillarruel@gmail.com>
2021-10-14 00:37:07 -0500

    FutureBuilder

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -17,21 +17,37 @@ class HomePage extends StatelessWidget {
   }
 
   Widget _list() {
-
-    print(menuProvider.options);
-
-    return ListView(
-      children: _listItems(),
+    return FutureBuilder(
+      future: menuProvider.loadData(),
+      initialData: const [],
+      builder: (context, AsyncSnapshot<List<dynamic>> snapshot) {
+        print(snapshot.data);
+        return ListView(
+          children: _listItems(snapshot.data),
+        );
+      },
     );
   }
 
-  List<Widget> _listItems() {
-    return const [
-      ListTile(title: Text('Hello world')),
-      Divider(),
-      ListTile(title: Text('Hello world')),
-      Divider(),
-      ListTile(title: Text('Hello world')),
-    ];
+  List<Widget> _listItems(List<dynamic>? data) {
+
+    final List<Widget> options = [];
+    
+    data?.forEach((item) {
+
+      final widgetTemp = ListTile(
+        leading: const Icon(Icons.account_circle, color: Colors.blue),
+        title: Text(item["texto"]),
+        trailing: const Icon(Icons.keyboard_arrow_right, color: Colors.blue),
+        onTap: () {},
+      );
+
+      options
+        ..add(widgetTemp)
+        ..add(const Divider());
+      
+    });
+
+    return options;
   }
 }
