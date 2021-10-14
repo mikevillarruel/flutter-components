a4a5b2be17ef42aab93508c9295cfccea83a94f7
Michael Villarruel <alejovillarruel@gmail.com>
2021-10-14 15:23:20 -0500

    MaterialPageRoute and Navigator

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_components/src/pages/alert_page.dart';
 import 'package:flutter_components/src/providers/menu_provider.dart';
+import 'package:flutter_components/src/utils/icon_string_util.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -21,25 +23,32 @@ class HomePage extends StatelessWidget {
       future: menuProvider.loadData(),
       initialData: const [],
       builder: (context, AsyncSnapshot<List<dynamic>> snapshot) {
-        print(snapshot.data);
         return ListView(
-          children: _listItems(snapshot.data),
+          children: _listItems(snapshot.data, context),
         );
       },
     );
   }
 
-  List<Widget> _listItems(List<dynamic>? data) {
+  List<Widget> _listItems(List<dynamic>? data, BuildContext context) {
 
     final List<Widget> options = [];
     
     data?.forEach((item) {
 
       final widgetTemp = ListTile(
-        leading: const Icon(Icons.account_circle, color: Colors.blue),
+        leading: getIcon(item['icon']),
         title: Text(item["texto"]),
         trailing: const Icon(Icons.keyboard_arrow_right, color: Colors.blue),
-        onTap: () {},
+        onTap: () {
+          final route = MaterialPageRoute(
+            builder: (context){
+              return const AlertPage();
+            }
+          );
+          Navigator.push(context, route);
+
+        },
       );
 
       options
