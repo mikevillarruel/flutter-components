7065cb594412b3ab04e5d5ba1de20d4ed1a59f34
Michael Villarruel <alejovillarruel@gmail.com>
2021-10-15 10:06:34 -0500

    CircleAvatar

--- lib/src/pages/avatar_page.dart
+++ lib/src/pages/avatar_page.dart
@@ -8,7 +8,32 @@ class AvatarPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Avatar Page'),
-      )
+        actions: <Widget>[
+          Container(
+            padding: const EdgeInsets.all(5.0),
+            child: const CircleAvatar(
+              backgroundImage: NetworkImage(
+                  'https://www.tiposde.com/wp-content/uploads/personajes-plano.jpg'),
+              radius: 25.0,
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(right: 10.0),
+            child: const CircleAvatar(
+              child: Text('MV'),
+              backgroundColor: Colors.white,
+            ),
+          )
+        ],
+      ),
+      body: const Center(
+        child: FadeInImage(
+          image: NetworkImage(
+              'https://www.tiposde.com/wp-content/uploads/personajes-plano.jpg'),
+          placeholder: AssetImage('assets/jar-loading.gif'),
+          fadeInDuration: Duration(milliseconds: 200),
+        ),
+      ),
     );
   }
 }
