261bd60667b8dad2394c2f307b6440da78a1fc55
Michael Villarruel <alejovillarruel@gmail.com>
2021-10-14 23:57:43 -0500

    AlertDialog

--- lib/src/pages/alert_page.dart
+++ lib/src/pages/alert_page.dart
@@ -15,6 +15,49 @@ class AlertPage extends StatelessWidget {
           Navigator.pop(context);
         },
       ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () => _showAlert(context),
+          style: ButtonStyle(
+            backgroundColor: MaterialStateProperty.all(Colors.green),
+            shape: MaterialStateProperty.all(const StadiumBorder()),
+          ),
+          child: const Text('Show Alert'),
+        ),
+      ),
     );
+  }
+
+  void _showAlert(BuildContext context) {
+    showDialog(
+        context: context,
+        barrierDismissible: false,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(20.0)),
+            title: const Text('Title'),
+            content: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: const <Widget>[
+                Text(
+                    'This is my content, please click on one of the buttons below'),
+                FlutterLogo(
+                  size: 100.0,
+                ),
+              ],
+            ),
+            actions: <Widget>[
+              TextButton(
+                  child: const Text('Cancel'),
+                  onPressed: () => Navigator.of(context).pop()),
+              TextButton(
+                  child: const Text('Ok'),
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  }),
+            ],
+          );
+        });
   }
 }
