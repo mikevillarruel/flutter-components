b21469226298449b117313e5e3def2137e8a9bd4
Michael Villarruel <alejovillarruel@gmail.com>
2021-10-16 00:10:19 -0500

    InfiniteScroll

--- lib/src/pages/listview_page.dart
+++ lib/src/pages/listview_page.dart
@@ -10,7 +10,21 @@ class ListPage extends StatefulWidget {
 }
 
 class _ListPageState extends State<ListPage> {
-  List<int> _numberList = [1, 2, 3, 4, 5];
+  final List<int> _numberList = [];
+  int _lastItem = 0;
+  ScrollController _scrollController = ScrollController();
+
+  @override
+  void initState() {
+    super.initState();
+    _addTen();
+    _scrollController.addListener(() {
+      if (_scrollController.position.pixels ==
+          _scrollController.position.maxScrollExtent) {
+        _addTen();
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -24,14 +38,26 @@ class _ListPageState extends State<ListPage> {
 
   Widget _createList() {
     return ListView.builder(
+      controller: _scrollController,
       itemCount: _numberList.length,
       itemBuilder: (BuildContext context, int index) {
         final int image = _numberList[index];
         return FadeInImage(
+          height: 300,
+          fit: BoxFit.cover,
+          fadeInDuration: const Duration(milliseconds: 200),
           placeholder: const AssetImage('assets/jar-loading.gif'),
           image: NetworkImage('https://picsum.photos/500/300?image=$image'),
         );
       },
     );
+  }
+
+  void _addTen() {
+    for (var i = 0; i < 10; i++) {
+      _lastItem++;
+      _numberList.add(_lastItem);
+    }
+    setState(() {});
   }
 }
